fc2923f5a9933ec924c23210148507ae23e6d92f
liuxu <liuxu@wxchina.com>
2026-08-04 15:52:15 +0800

    1.删除无用的代码

--- sword/src/args/arguments.rs
+++ sword/src/args/arguments.rs
@@ -6,39 +6,3 @@ pub struct LoadOptions {
     #[arg(long, short = 'c')]
     command: String,
 }
-
-#[cfg(test)]
-mod tests {
-    use super::LoadOptions;
-    use clap::Parser;
-
-    #[test]
-    fn parses_target_pid() {
-        let options = LoadOptions::try_parse_from(["sword", "--target-pid", "42"]).unwrap();
-
-        assert_eq!(options.target_pid, Some(42));
-        assert_eq!(options.target_name, None);
-    }
-
-    #[test]
-    fn parses_target_name() {
-        let options =
-            LoadOptions::try_parse_from(["sword", "--target-name", "ccm-server"]).unwrap();
-
-        assert_eq!(options.target_pid, None);
-        assert_eq!(options.target_name.as_deref(), Some("ccm-server"));
-    }
-
-    #[test]
-    fn rejects_pid_and_name_together() {
-        let result = LoadOptions::try_parse_from([
-            "sword",
-            "--target-pid",
-            "42",
-            "--target-name",
-            "ccm-server",
-        ]);
-
-        assert!(result.is_err());
-    }
-}
